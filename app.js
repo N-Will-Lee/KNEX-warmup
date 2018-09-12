04481a19e413c70b43caa6731ef722de3f4897f6
Nicholas Lee <nicholaslee@Nicholass-MacBook-Air-2.local>
2018-09-12 09:35:31 -0600

    updated migration file

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 // const family = require('./data.js');
 const app = express();
 const port = process.env.port || 3000;
+const queries = require('./queries');
 
 // app.get('/', (request, response) => {
 //     response.send(family);
@@ -12,4 +13,9 @@ const port = process.env.port || 3000;
 
 app.listen(port, () => {
     console.log(`listening on ${port}`)
+    console.log(queries);
+})
+app.get('/', (request, res) => {
+    res.send('the route worked!!');
+    queries.getAll().then(response => res.send(response))
 })
---
+++ knexfile.js
@@ -0,0 +1,18 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: 'postgresql://localhost/DATA_NICK'
+  },
+
+  staging: {
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL
+  }
+
+};
